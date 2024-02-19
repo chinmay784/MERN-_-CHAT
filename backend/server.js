7c3b5d8b27edb48cb3bd44ccd6d398ae126de53e
chinmaypuhan <chinmaypuhan420@gmail.com>
2024-02-19 12:54:24 +0530

    6th complite

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const cookieparser = require("cookie-parser")
+const path = require("path")
+
 const authRoute = require("./routes/auth_route");
 const messageRoute = require("./routes/messageRoute");
 const userroute = require("./routes/userRoutes")
@@ -9,13 +10,29 @@ const { app, server } = require("./sockets/socket");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 
+
+// const __dirname = path.resolve()
+
+
 app.use(express.json());
 app.use(cookieparser());
 
 
+
+
+
+
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
-app.use("/api/users",userroute)
+app.use("/api/users",userroute);
+
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+})
 
 server.listen(port,() =>{
     connectTOmongoDB();
